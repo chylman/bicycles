9df0fd42ad2194569abc3f14e379d840bfe3251c
Илья Афанасьев < >
2021-05-15 00:20:50 +0300

    Правки 2.0

--- build/js/main.js
+++ build/js/main.js
@@ -1,44 +1,62 @@
 'use strict';
 
-const pageBody = document.querySelector('.page-body');
-const pageHeader = document.querySelector('.page-header');
-const navigationToggle = document.querySelector('.main-nav__toggle');
-const mainNavigation = document.querySelector('.main-nav');
-const mainNavLinksArray = Array.from(mainNavigation.querySelectorAll('a'));
+(function () {
+  const pageBody = document.querySelector('.page-body');
+  const pageHeader = document.querySelector('.page-header');
+  const navigationToggle = document.querySelector('.main-nav__toggle');
+  const mainNavigation = document.querySelector('.main-nav');
+  const mainNavLinksArray = Array.from(mainNavigation.querySelectorAll('a'));
 
-pageHeader.classList.remove('page-header--no-js');
-pageHeader.classList.add('page-header--js');
-mainNavigation.classList.add('main-nav--close');
-
-navigationToggle.addEventListener('click', () => {
-  if (mainNavigation.classList.contains('main-nav--close')) {
-    mainNavigation.classList.remove('main-nav--close');
-    mainNavigation.classList.add('main-nav--open');
-    pageBody.style.overflow = 'hidden';
-  } else {
-    mainNavigation.classList.add('main-nav--close');
-    mainNavigation.classList.remove('main-nav--open');
-    pageBody.style.overflow = '';
+  if (pageHeader) {
+    pageHeader.classList.remove('page-header--no-js');
+    pageHeader.classList.add('page-header--js');
   }
-});
 
-const scrollTo = (element) => {
-  window.scroll({
-    left: 0,
-    top: element.offsetTop,
-    behavior: 'smooth',
-  })
-}
+  if (mainNavigation) {
+    mainNavigation.classList.add('main-nav--close');
+  }
 
-const onMainNavLinksClick = (evt) => {
-  evt.preventDefault();
-  console.log(evt);
-  scrollTo(document.querySelector(evt.target.hash));
-  mainNavigation.classList.add('main-nav--close');
-  mainNavigation.classList.remove('main-nav--open');
-  pageBody.style.overflow = '';
-}
+  const onNavigationToggleClick = () => {
+    if (mainNavigation && pageBody) {
+      if (mainNavigation.classList.contains('main-nav--close')) {
+        mainNavigation.classList.remove('main-nav--close');
+        mainNavigation.classList.add('main-nav--open');
+        pageBody.classList.add('overflow-hidden');
+      } else {
+        mainNavigation.classList.add('main-nav--close');
+        mainNavigation.classList.remove('main-nav--open');
+        pageBody.classList.remove('overflow-hidden');
+      }
+    }
+  };
 
-mainNavLinksArray.forEach(element => {
-  element.addEventListener('click', onMainNavLinksClick);
-});
+  if (navigationToggle) {
+    navigationToggle.addEventListener('click', onNavigationToggleClick);
+  }
+
+  const scrollTo = (element) => {
+    window.scroll({
+      left: 0,
+      top: element.offsetTop,
+      behavior: 'smooth',
+    });
+  };
+
+  const onMainNavLinksClick = (evt) => {
+    evt.preventDefault();
+    scrollTo(document.querySelector(evt.target.hash));
+
+    if (mainNavigation) {
+      mainNavigation.classList.add('main-nav--close');
+      mainNavigation.classList.remove('main-nav--open');
+    }
+    if (pageBody) {
+      pageBody.classList.remove('overflow-hidden');
+    }
+  };
+
+  mainNavLinksArray.forEach(element => {
+    element.addEventListener('click', onMainNavLinksClick);
+  });
+
+}());
